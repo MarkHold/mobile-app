d7db6d19e4a5a7b147be70734f9cc223318fc4c4
Markus Lykonhold <lykonhold@gmail.com>
2023-04-08 14:03:27 +0200

    initial commit

---
+++ app/_layout.js
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router';
+import { SafeAreaInsetsContext } from 'react-native-safe-area-context';
+
+const Layout = () => {
+    return<Stack />;
+}
+
+export default Layout;
